071444a063066823da99fb57e9bb2f3eb532f77d
nopaluhuy <nopaluhuy07@gmail.com>
2026-01-19 20:07:28 +0700

    update fullscreen

--- resources/views/layouts/fullscreen-layout.blade.php
+++ resources/views/layouts/fullscreen-layout.blade.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ $title ?? 'Dashboard' }} | TailAdmin</title>
+    <title>{{ $title ?? 'Dashboard' }} | WokaPayment</title>
+
+    <link rel="icon" type="image/png" href="{{ asset('assets/logoPY.png') }}">
 
     <!-- Vite -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
